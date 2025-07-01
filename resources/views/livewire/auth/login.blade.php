cb2280d684e5f94f8e225d0b5677bf606a720a48
Douglas Cockerham <padretres@gmail.com>
2025-07-01 12:13:46 -0400

    Initial

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -45,8 +45,8 @@
 
     @if (Route::has('register'))
         <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-            {{ __('Don\'t have an account?') }}
-            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
+            {{ __('Don\'t have an account?') }} <i class="text-white font-bold">Too bad.</i>
+{{--            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>--}}
         </div>
     @endif
 </div>
