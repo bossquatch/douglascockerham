310262fa4629c9da4f50c6259957469243551d77
Douglas Cockerham <padretres@gmail.com>
2026-06-12 09:00:52 -0400

    G-300 page updated, G400 added

--- resources/views/g300.blade.php
+++ resources/views/g300.blade.php
@@ -144,12 +144,6 @@
                         <a href="https://forms.cloud.microsoft/g/EE161xizuA" class="text-gray-400 hover:text-gray-300">Registration for ICS-400 June 15 & 16, 2026</a>
 
                     </dt>
-                    <dt class="inline font-semibold text-white">
-                        <svg class="absolute top-1 left-1 size-5 text-indigo-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                            <path d="M15.98 1.804a1 1 0 0 0-1.96 0l-.24 1.192a1 1 0 0 1-.784.785l-1.192.238a1 1 0 0 0 0 1.962l1.192.238a1 1 0 0 1 .785.785l.238 1.192a1 1 0 0 0 1.962 0l.238-1.192a1 1 0 0 1 .785-.785l1.192-.238a1 1 0 0 0 0-1.962l-1.192-.238a1 1 0 0 1-.785-.785l-.238-1.192ZM6.949 5.684a1 1 0 0 0-1.898 0l-.683 2.051a1 1 0 0 1-.633.633l-2.051.683a1 1 0 0 0 0 1.898l2.051.684a1 1 0 0 1 .633.632l.683 2.051a1 1 0 0 0 1.898 0l.683-2.051a1 1 0 0 1 .633-.633l2.051-.683a1 1 0 0 0 0-1.898l-2.051-.683a1 1 0 0 1-.633-.633L6.95 5.684ZM13.949 13.684a1 1 0 0 0-1.898 0l-.184.551a1 1 0 0 1-.632.633l-.551.183a1 1 0 0 0 0 1.898l.551.183a1 1 0 0 1 .633.633l.183.551a1 1 0 0 0 1.898 0l.184-.551a1 1 0 0 1 .632-.633l.551-.183a1 1 0 0 0 0-1.898l-.551-.184a1 1 0 0 1-.633-.632l-.183-.551Z" />
-                        </svg>
-                        <a href="{{route('g400')}}" class="text-gray-400 hover:text-gray-300">G-400 Class Materials</a>
-                    </dt>
                 </div>
                 <div class="relative pl-9">
                     <dt class="inline font-semibold text-white">
